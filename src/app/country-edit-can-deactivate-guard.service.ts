c30c3028407d03accb55f25d57d1a0238440ec37
RavbarKoda <70749432+RavbarKoda@users.noreply.github.com>
2023-09-29 21:41:21 +0200

    1 file modified

--- src/app/country-edit-can-deactivate-guard.service.ts
+++ src/app/country-edit-can-deactivate-guard.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { HostListener, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RouterStateSnapshot } from '@angular/router';
 import { CountryEditComponent } from './country/country-list/edit/country.edit.component';
@@ -6,8 +6,12 @@ import { DialogService } from './dialog.service';
 
 @Injectable()
 export class CountryEditCanDeactivateGuard {
-  constructor(private dialogService: DialogService) { }
-  canDeactivate(component: CountryEditComponent, state: RouterStateSnapshot): Observable<boolean> | boolean {
+  constructor(private dialogService: DialogService) {}
+  @HostListener('window:beforeunload')
+  canDeactivate(
+    component: CountryEditComponent,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
     let url: string = state.url;
     console.log('Url: ' + url);
     if (!component.isUpdating && component.countryForm.dirty) {
